ed0f972979bfea45cf96237f1d841125eba563d1
Shabbir Rayeen <sawone2893@gmail.com>
2023-09-23 18:22:22 +0530

    Updated new changes

--- test.setup.ts
+++ test.setup.ts
@@ -1,24 +1,21 @@
-import { WebActions } from "./src/base/webActions";
-import { Before, BeforeAll, AfterAll, setDefaultTimeout } from "@cucumber/cucumber";
+import { PlaywrightActions } from "./src/base/playwrightActions";
+import { BeforeAll, AfterAll, setDefaultTimeout } from "@cucumber/cucumber";
 import * as config from "./src/config/config.json";
 import { readFileSync } from "fs";
 const chai = require('chai');
-setDefaultTimeout(2 * 60000);
+setDefaultTimeout(120000);
 
 declare global {
     var actionDriver: any;
 }
 BeforeAll(async () => {
     global.expect_ = chai.expect;
-    global.actionDriver = new WebActions();
-    await global.actionDriver.initializesBrowser(config.browserName, config.isBrowserModeHeadless);
-});
-
-Before(async () => {
+    global.actionDriver = new PlaywrightActions(config.browserType);
+    await global.actionDriver.initialize(config.browserChannelName, config.isBrowserModeHeadless);
     await global.actionDriver.openURL(config.appURL);
 });
 
 AfterAll(async () => {
-    await global.actionDriver.closePage();
-    await global.actionDriver.closeBrowserContext();
+    //await global.actionDriver.closePage();
+    await global.actionDriver.closeBrowser();
 });
